d0a89c08493908b05b4b4b910a83815f5bd9896a
James <photodow@gmail.com>
2014-04-02 02:15:46 -0400

    started html/css on content

--- app/views/home.php
+++ app/views/home.php
@@ -1,19 +1,70 @@
-<section id="welcome">
+<section class="welcome">
 	<div class="innerWrapper">
     	Welcome Message
     </div>
 </section>
-<section id="features">
+<section class="features">
 	<div class="innerWrapper">
-	Features Section
+		<ul>
+        	<li class="linebreak">
+            	<h2>
+                	<i class="fa fa-search"></i>
+                    Title Goes Here
+                </h2>
+                <p>Caring beyond the nature of the emphasis and profound that produced it can we try to be better is very minimalist way something memorable and developing anything that, and simplicity is.</p>
+            </li>
+        	<li>
+            	<h2>
+                	<i class="fa fa-search"></i>
+                    Title Goes Here
+                </h2>
+                <p>Caring beyond the nature of the emphasis and profound that produced it can we try to be better is very minimalist way something memorable and developing anything that, and simplicity is.</p>
+            </li>
+        	<li>
+            	<h2>
+                	<i class="fa fa-search"></i>
+                    Title Goes Here
+                </h2>
+                <p>Caring beyond the nature of the emphasis and profound that produced it can we try to be better is very minimalist way something memorable and developing anything that, and simplicity is.</p>
+            </li>
+        	<li class="linebreak">
+            	<h2>
+                	<i class="fa fa-search"></i>
+                    Title Goes Here
+                </h2>
+                <p>Caring beyond the nature of the emphasis and profound that produced it can we try to be better is very minimalist way something memorable and developing anything that, and simplicity is.</p>
+            </li>
+        	<li>
+            	<h2>
+                	<i class="fa fa-search"></i>
+                    Title Goes Here
+                </h2>
+                <p>Caring beyond the nature of the emphasis and profound that produced it can we try to be better is very minimalist way something memorable and developing anything that, and simplicity is.</p>
+            </li>
+        	<li>
+            	<h2>
+                	<i class="fa fa-search"></i>
+                    Title Goes Here
+                </h2>
+                <p>Caring beyond the nature of the emphasis and profound that produced it can we try to be better is very minimalist way something memorable and developing anything that, and simplicity is.</p>
+            </li>
+        </ul>
     </div>
 </section>
-<section id="about">
+<section class="about">
 	<div class="innerWrapper">
-	About Section
+        <h2>About</h2> <!-- TO DO: Add custom LiveSlide Font Icon -->
+        <div class="two-col">
+            <p>[Dummy Text] : Their design is in many of care. That's a product. Great design as possible advantage. It is intended to mean nothing must be kept. It and restrained, that the defining qualities are indifferent to be used.</p>
+            <p>Zero out some things which are seldom better. Good design is made to be, but very often diagrams rely solely on going until you go, lying comes from a very minimalist way. Zero out your perception of material in simplicity is longlasting. Design is incredibly challenging. We try to the particular mode of seeing and i hope that it does not an object and product is honest!</p>
+            <p>Designing and often design is making something works of simplicity is not make a product useful. The way beyond the content. People know how complicated problems without letting people know how complicated problems without letting people know how it really is.</p>
+            <p>One level of british culture, well, measured more in efficiency. That's an appearance game we're surrounded by seem trivial. I think there are about powerpoint should therefore be used solely on the particular mode of information overload, well, but that changes function like tools.</p>
+            <p>Caring beyond the nature of the emphasis and profound that produced it can we try to be better is very minimalist way something memorable and developing anything that, and simplicity is. Products have always thought about powerpoint should be both neutral and ornamentation. Good design should be both neutral and original thinking.</p>
+			<p>Todays graphics use: ease and some universal cognitive style of analysis. I hope that we need that is not an interesting thing about an exploration of information and aesthetic. The essential is unobtrusive. The traditions of simplicity is derived from the last detail. The computer. Good design. People know how it does not only functional, not attempt to.</p>
+        </div>
     </div>
 </section>
-<section id="contact">
+<section class="contact">
 	<div class="innerWrapper">
 	Contact Section
     </div>
