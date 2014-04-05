ceb338c700bcb2ca89e2d10cbb08bf72de88f52e
James <photodow@gmail.com>
2014-04-05 17:26:30 -0400

    finished HTML/CSS profile view

--- app/views/profile.php
+++ app/views/profile.php
@@ -1,5 +1,27 @@
 <section class="profile left">
-	<header></header>
+	<header>
+    	<img src="/img/noProfileImg.png" alt="The User's Profile Image" />
+        <div class="centVert">
+            <h2>Diane Gordon</h2>
+            <p class="headline">Radio Show Host at WWPR 1490</p>
+            <aside class="social">
+                <ul>
+                    <li><a href="mailto:email@server.com" class="email" title="Email"><i class="icon-envelope"></i></a></li>
+                    <li><a href="#" class="website" title="Website"><i class="icon-globe"></i></a></li>
+                    <li><a href="https://www.facebook.com/" class="facebook" title="Facebook"><i class="icon-facebook-square"></i></a></li>
+                    <li><a href="https://twitter.com/" class="twitter" title="Twitter"><i class="icon-twitter"></i></a></li>
+                    <li><a href="https://plus.google.com/‎" class="googleplus" title="Google Plus"><i class="icon-google-plus-square"></i></a></li>
+                    <li><a href="https://www.linkedin.com/" class="linkedin" title="LinkedIn"><i class="icon-linkedin-square"></i></a></li>
+                </ul>
+            </aside>
+        </div>
+        <aside class="stats">
+        	<ul>
+            	<li class="followers"><i class="icon-group"></i> <span class="num">5 Follower<span class="plural">s</span></li>
+            	<li class="presentation"><i class="icon-youtube-play"></i> <span class="num">10</span> Presentation<span class="plural">s</span></li>
+            </ul>
+        </aside>
+    </header>
     <article>
     	<h3>About</h3>
         <p>Diane hosts a radio show to give financial advice to anyone who will listen. Videos are also posted on her website so people can watch her recording her show and view various captions while listening. LiveSlides would be helpful to those who listen to Diane’s show because she could easily set up her slides for listeners to view as she speaks live on the radio. When we implement the voiceover and video into the web application, Diane would be able to use the paid version on her website so people could view the presentation after the fact, similar to the videos she currently has except it would be more visually appealing and easier to follow along.</p>
