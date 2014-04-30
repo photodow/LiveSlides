a241b7ff88640d230e7c626cfe5c382e2777367b
James <photodow@gmail.com>
2014-04-30 16:03:54 -0400

    slide js

---
+++ app/views/localScript/slide.php
@@ -0,0 +1,2 @@
+<script text="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<script text="text/javascript" src="/js/liveslides.js"></script>
